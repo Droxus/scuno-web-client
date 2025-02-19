86dde72d11d27f8305f7d2774234c74914d053b4
oleksandr-krasnovidov <alexredov@gmail.com>
2025-02-19 23:30:53 +0000

    toncoin manifest created

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import "./App.css";
+import { TonConnectButton, CHAIN } from "@tonconnect/ui-react";
+import { useTonConnect } from "./hooks/useTonConnect";
 
 function App() {
   function onBuyLevel(level: number) {
     console.log("Buying level", level);
   }
 
+  const network = useTonConnect().network;
+
   return (
     <>
+      <TonConnectButton />
+      <span>
+        {network ? (network === CHAIN.MAINNET ? "mainnet" : "testnet") : "N/A"}
+      </span>
       <button onClick={() => onBuyLevel(1)}>Buy level 1</button>
       <button onClick={() => onBuyLevel(2)}>Buy level 2</button>
       <button onClick={() => onBuyLevel(3)}>Buy level 3</button>
