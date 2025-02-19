bf122aeac5c230c2cf2d9493686b9141bdec06f4
oleksandr-krasnovidov <alexredov@gmail.com>
2025-02-19 23:59:42 +0000

    vite-config base added

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { nodePolyfills } from "vite-plugin-node-polyfills";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), nodePolyfills()],
+  base: "/scuno-web-client/",
   build: {
     outDir: "docs", // Set the output directory to 'docs'
   },
